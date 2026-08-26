f6e504192047b900b8ef405914b9436fcdeec1bf
lizan <lizansarkar16@gmail.com>
2026-08-26 23:24:09 +0600

    feat: add Feature section with 360 panoramic view, spatial UI, and in-world hotspot

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Hero'
+import Feature from './Feature'
 
 function Home() {
   return (
     <div>
       {/* <ExampleUsage /> */}
       <Hero />
+      <Feature />
     </div>
   )
 }
